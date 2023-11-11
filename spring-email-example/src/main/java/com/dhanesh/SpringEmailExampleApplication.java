e9f95ee88515d527cc67c934d347f2daf22ae5f4
Dhanesh Yadav <88324925+Always-Gamerz@users.noreply.github.com>
2023-11-11 17:13:03 +0530

    Add files via upload

---
+++ spring-email-example/src/main/java/com/dhanesh/SpringEmailExampleApplication.java
@@ -0,0 +1,13 @@
+package com.dhanesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmailExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmailExampleApplication.class, args);
+	}
+
+}
